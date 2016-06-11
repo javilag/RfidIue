d3a25356b8596310577ad860885a0cd895516352
javilag <PRACTICAS@pc136.salas.local>
2016-06-11 09:38:53 -0500

    i think is the db

--- controllers/person/personController.js
+++ controllers/person/personController.js
@@ -1,5 +1,4 @@
 var express = require('express');
 var router = express.Router();
-var pg = require('pg');
 var path = require('path');
 var conString = require(path.join(__dirname, '../', '../', 'models/database.js'));
--- models/database.js
+++ models/database.js
@@ -1,3 +1,3 @@
-var connectionString = process.env.DATABASE_URL || 'postgres://postgres:password@localhost:5432/RFID';
+var connectionString = process.env.DATABASE_URL || 'postgres://ajyemphrgycrsq:8PcyTaVW7X4QdiMJiUdf2qubc-@ec2-54-243-248-181.compute-1.amazonaws.com:5432/d3s6831b5o8cck';
 
 module.exports = connectionString;
