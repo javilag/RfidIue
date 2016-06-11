514ea375dc493cc53caf33d434e0e14cc1efaaac
javilag <PRACTICAS@pc136.salas.local>
2016-06-11 09:48:20 -0500

    asdggasdfs

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var pg = require('pg');
 var path = require('path');
-var conString = require(path.join(__dirname, '../', '../', '../models/database.js'));
+var conString = require(path.join(__dirname, '../', 'database.js'));
 
 
 router.get('/', function(req, res, next) {
