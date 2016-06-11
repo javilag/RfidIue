93b6d429e779eca1e311f801d11df56e9fe519b4
javilag <PRACTICAS@pc136.salas.local>
2016-06-11 09:42:00 -0500

    no message

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var pg = require('pg');
 var path = require('path');
-var conString = require(path.join(__dirname, '../', '../', 'RfidIue/models/database.js'));
+var conString = require(path.join(__dirname, '../', '../', '../models/database.js'));
 
 
 router.get('/', function(req, res, next) {
